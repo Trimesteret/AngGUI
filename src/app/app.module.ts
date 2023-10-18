df21e978e9214ed6af3499dbafc2e3c52b562988
Thomas Andersen <coolfatboy02@gmail.com>
2023-10-18 10:16:12 +0200

    SW-43 * Merged and fix issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule } from "@angular/material/button";
-import { MatInputModule } from "@angular/material/input";
-import { FlexModule } from "@angular/flex-layout";
-import { CreateItemsComponent } from "./create-items/create-items.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { MatFormFieldModule } from "@angular/material/form-field";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FlexModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FrontPageComponent } from './front/front-page/front-page.component';
+import { FrontBasicLayoutComponent } from './front/front-basic-layout/front-basic-layout.component';
+import { FrontHeaderComponent } from './front/front-header/front-header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		CreateItemsComponent
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		MatButtonModule,
-		MatInputModule,
-		FlexModule,
-		FormsModule,
-		ReactiveFormsModule,
-		HttpClientModule,
-		MatFormFieldModule
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    FrontPageComponent,
+    FrontBasicLayoutComponent,
+    FrontHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    FlexModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/warehouse/warehouse-routing.module.ts
+++ src/app/warehouse/warehouse-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { CreateItemComponent } from './create-item/create-item.component';
 
 const routes: Routes = [
+  { path: 'create-item', component: CreateItemComponent },
   { path: 'home', component: HomePageComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
