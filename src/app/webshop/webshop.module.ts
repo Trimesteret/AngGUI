7ce95aa2e61463edfc0c13112317d25143afd7ff
AndreasBM1 <Ak42an@student.aau.dk>
2023-10-20 09:22:24 +0200

    SW 44 * Made a basic checkoutForm

--- src/app/webshop/webshop.module.ts
+++ src/app/webshop/webshop.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { WebshopRoutingModule } from './webshop-routing.module';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    CheckoutComponent
   ],
   imports: [
     CommonModule,
-    WebshopRoutingModule
+    WebshopRoutingModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FlexModule,
+    MatButtonModule
   ]
 })
 export class WebshopModule { }
