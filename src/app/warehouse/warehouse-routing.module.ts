2c6c803930f9bf7078f6c8765fd53aaf0568ca47
Thomas Andersen <coolfatboy02@gmail.com>
2023-11-27 00:11:38 +0100

    SW-117 * Added role authorization

--- src/app/warehouse/warehouse-routing.module.ts
+++ src/app/warehouse/warehouse-routing.module.ts
@@ -4,11 +4,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { CreateItemComponent } from './create-item/create-item.component';
 import { CreateInboundOrderComponent } from './create-inbound-order/create-inbound-order.component';
 import { CreateEditUsersComponent } from './create-edit-users/create-edit-users.component';
+import { authGuard } from '../guards/auth.guard';
+import { roleGuard } from '../guards/role.guard';
+import { Roles } from '../shared/enums/roles';
 
 const routes: Routes = [
-  { path: 'createInboundOrder', component: CreateInboundOrderComponent },
-  { path: 'create-edit-items', component: CreateItemComponent },
-  { path: 'create-edit-users', component: CreateEditUsersComponent },
+  { path: 'createInboundOrder', component: CreateInboundOrderComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Roles.Admin } },
+  { path: 'create-edit-items', component: CreateItemComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Roles.Admin } },
+  { path: 'create-edit-users', component: CreateEditUsersComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Roles.Admin } },
   { path: 'home', component: HomePageComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
