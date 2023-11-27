dbf63e79f5325f339bb3157764cf8aab97ff8208
Daniel <danbodker@gmail.com>
2023-11-27 18:13:22 +0100

    basket routing

--- src/app/webshop/webshop-routing.module.ts
+++ src/app/webshop/webshop-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { ItemDisplayComponent } from './item-display/item-display.component';
+import {BasketComponent} from "./basket/basket.component";
 
 const routes: Routes = [
   { path: 'checkout', component: CheckoutComponent },
+  { path: 'basket', component: BasketComponent },
   { path: 'item/:id', component: ItemDisplayComponent },
   { path: '', component: CatalogueComponent }
 ];
