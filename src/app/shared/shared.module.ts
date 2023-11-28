654d01d097168a590aaffac5fce4b2ed68b0f9a2
Thomas Andersen <coolfatboy02@gmail.com>
2023-11-28 22:02:21 +0100

    keyValuePipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { KeyValuePipe } from './pipes/key-value.pipe';
 
 
 @NgModule({
   declarations: [
-
+    KeyValuePipe
   ],
   imports: [
     CommonModule,
