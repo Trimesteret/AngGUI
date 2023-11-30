d1849d6768511e69ad77179c63a6e258c07303c8
Daniel <danbodker@gmail.com>
2023-11-30 15:52:58 +0100

    PR changes
    - Removed ngFor for form fields and radio buttons
    - Fixed spaces around equals
    - Added formbuilder

--- src/app/webshop/checkout/checkout.component.ts
+++ src/app/webshop/checkout/checkout.component.ts
@@ -1,71 +1,50 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent {
-  checkoutForm = new FormGroup({
-    name: new FormControl(''),
-    address: new FormControl(''),
-    phoneNumber: new FormControl(''),
-    city: new FormControl(''),
-    country: new FormControl(''),
-    email: new FormControl(''),
-    deliveryMethod: new FormControl(''),
-    postcode: new FormControl(''),
-    termsCheckbox: new FormControl(''),
-    newsletterCheckbox: new FormControl('')
-  });
+export class CheckoutComponent implements OnInit{
+  checkoutForm: FormGroup | undefined;
 
+  constructor(private fb: FormBuilder) { }
+  ngOnInit() : void {
+    this.checkoutForm = this.fb.group({
+      // Define your form controls here using FormBuilder
+      // Example: 'name': [initialValue, [validators]]
+      'name': ['', Validators.required],
+      'phoneNumber': ['', Validators.required],
+      'email': ['', Validators.required],
+      'address': ['', Validators.required],
+      'country': ['Danmark', Validators.required],
+      'postcode': ['', Validators.required],
+      'city': ['', Validators.required],
+      'deliveryMethod': ['', Validators.required],
+      'termsCheckbox': [false, Validators.required],
+      'newsletterCheckbox': [false],
+    });
+  }
   deliveryMethods = [
     { value: 'pickup', cost: 0, label: 'Afhentning i Butik' },
     { value: 'postnord', cost: 59, label: 'Postnord Hjemmelevering' },
     { value: 'gls', cost: 59, label: 'GLS' },
     { value: 'dao', cost: 59, label: 'DAO' }
   ];
-
-  addressFields = [
-    { title: 'name', placeholder: 'Navn' },
-    { title: 'phoneNumber', placeholder: 'Telefon Nummer' },
-    { title: 'email', placeholder: 'E-mail' },
-    { title: 'address', placeholder: 'Adresse' },
-    { title: 'city', placeholder: 'By' },
-    { title: 'country', placeholder: 'Land' }
-  ];
-
-
   // Placeholder. This must be fetched from the actual basket, so a service should probably be made to get that information
   basketContent = [
-    {name: 'Ugandisk Vin', price: '200', imageUrl: "assets/PeanutNoar.jfif"},
-    {name: 'God Gammeldags Rødvin', price: '300', imageUrl: "assets/PeanutNoar.jfif"},
-    {name: 'God Gammeldags Rødvin', price: '300', imageUrl: "assets/PeanutNoar.jfif"},
-    {name: 'God Gammeldags Rødvin', price: '300', imageUrl: "assets/PeanutNoar.jfif"},
-    {name: 'God Gammeldags Rødvin', price: '300', imageUrl: "assets/PeanutNoar.jfif"},
-    {name: 'God Gammeldags Rødvin', price: '300', imageUrl: "assets/PeanutNoar.jfif"},
-    {name: 'Viktor Special', price: '15', imageUrl: "assets/PeanutNoar.jfif"}
-    ];
+    { name: 'Ugandisk Vin', price: '200', imageUrl: 'assets/PeanutNoar.jfif' },
+    { name: 'God Gammeldags Rødvin', price: '300', imageUrl: 'assets/PeanutNoar.jfif' },
+    { name: 'God Gammeldags Rødvin', price: '300', imageUrl: 'assets/PeanutNoar.jfif' },
+    { name: 'God Gammeldags Rødvin', price: '300', imageUrl: 'assets/PeanutNoar.jfif' },
+    { name: 'God Gammeldags Rødvin', price: '300', imageUrl: 'assets/PeanutNoar.jfif' },
+    { name: 'God Gammeldags Rødvin', price: '300', imageUrl: 'assets/PeanutNoar.jfif' },
+    { name: 'Viktor Special', price: '15', imageUrl: 'assets/PeanutNoar.jfif' }
+  ];
+  createCheckout():void{
+    console.log(this.checkoutForm);
+  }
   goToPayment():void{
     console.log('Hello Uniconta, please help :(');
-  }
-
-  // Should probably be in a basket/item service
-  calculateTotalPrice(basketContent: { name: string, price: string }[]): number {
-    let totalPrice = 0;
-
-    for (const item of basketContent) {
-      const itemPrice = parseFloat(item.price);
-
-      if (!isNaN(itemPrice)) {
-        totalPrice += itemPrice;
-      } else {
-        console.error(`Invalid price for item '${item.name}': ${item.price}`);
-      }
-    }
-    return totalPrice;
-  }
-  createCheckout():void{
-    console.log(this.checkoutForm.value);
   }
 }
