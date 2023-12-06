3bf1239792a095f5938a5415063e6ae09b131093
Daniel <danbodker@gmail.com>
2023-12-06 16:05:56 +0100

    Fixed total price

--- src/app/webshop/checkout/checkout.component.ts
+++ src/app/webshop/checkout/checkout.component.ts
@@ -11,6 +11,7 @@ import { OrderService } from '../../shared/services/order/order.service';
 export class CheckoutComponent implements OnInit{
   checkoutForm: FormGroup | undefined;
   constructor(private fb: FormBuilder, private orderService: OrderService) { }
+  basketContent: OrderLineDto[];
   ngOnInit() : void {
     this.checkoutForm = this.fb.group({
       'name': ['', Validators.required],
@@ -24,6 +25,16 @@ export class CheckoutComponent implements OnInit{
       'termsCheckbox': [false, Validators.required],
       'newsletterCheckbox': [false],
     });
+    this.basketContent = this.getBasketItems();
+    this.calculateTotalPrice(this.basketContent);
+  }
+  calculateTotalPrice(orderLines: OrderLineDto[]): number {
+    let total = 0;
+    orderLines.forEach(function (value : OrderLineDto){
+      total += (value.price * value.quantity);
+    });
+    this.orderService.getCurrentPurchaseOrder().totalPrice = total;
+    return total;
   }
   getTotalPrice(): number {
     return this.orderService.getCurrentPurchaseOrder().totalPrice;
@@ -31,7 +42,6 @@ export class CheckoutComponent implements OnInit{
   getBasketItems(): OrderLineDto[] {
     return this.orderService.getCurrentPurchaseOrder().orderLines;
   }
-  basketContent = this.getBasketItems();
   public goToPayment(): void {
     // Not implemented, should send the order to API
   }
