a7cd375a74e6d1b8d1cf56ab446058984c390816
Barnet <55490372+Thekid2002@users.noreply.github.com>
2023-12-09 15:17:04 +0100

    Sw 131 (#35)
    * SW-131 * Beginning enums page
    
    * SW-131 * Enum change finished

--- src/app/warehouse/warehouse-routing.module.ts
+++ src/app/warehouse/warehouse-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
 import { CreateInboundOrderComponent } from './create-inbound-order/create-inbound-order.component';
 import { authGuard } from '../guards/auth.guard';
 import { roleGuard } from '../guards/role.guard';
@@ -9,6 +8,8 @@ import { BrowseItemsComponent } from './browse-items/browse-items.component';
 import { BrowseUsersComponent } from './browse-users/browse-users.component';
 import { CreateEditItemComponent } from './create-edit-item/create-edit-item.component';
 import { CreateEditUserComponent } from './create-edit-user/create-edit-user.component';
+import { SettingsComponent } from './settings/settings.component';
+import { CreateEditEnumComponent } from './create-edit-enum/create-edit-enum.component';
 import { BrowseSuppliersComponent } from './browse-suppliers/browse-suppliers.component';
 import { CreateEditSupplierComponent } from './create-edit-supplier/create-edit-supplier.component';
 
@@ -21,10 +22,12 @@ const routes: Routes = [
   { path: 'create-supplier', component: CreateEditSupplierComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Role.Admin } },
   { path: 'edit-supplier/:id', component: CreateEditSupplierComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Role.Admin } },
   { path: 'create-user', component: CreateEditUserComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Role.Admin } },
-  { path: 'edit-item/:id', component: CreateEditItemComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Role.Admin } },
   { path: 'create-item', component: CreateEditItemComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Role.Admin } },
-  { path: 'home', component: HomePageComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'edit-item/:id', component: CreateEditItemComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Role.Admin } },
+  { path: 'edit-enum/:id', component: CreateEditEnumComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Role.Admin } },
+  { path: 'create-enum', component: CreateEditEnumComponent, canActivate: [authGuard, roleGuard], data: { expectedRole: Role.Admin } },
+  { path: 'settings', component: SettingsComponent },
+  { path: '', redirectTo: 'settings', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/warehouse/warehouse.module.ts
+++ src/app/warehouse/warehouse.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './home-page/home-page.component';
 import { WarehouseRoutingModule } from './warehouse-routing.module';
 import { CreateInboundOrderComponent } from './create-inbound-order/create-inbound-order.component';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -28,6 +27,8 @@ import { BrowseItemsComponent } from './browse-items/browse-items.component';
 import { CreateEditUserComponent } from './create-edit-user/create-edit-user.component';
 import { CreateEditItemComponent } from './create-edit-item/create-edit-item.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { SettingsComponent } from './settings/settings.component';
+import { CreateEditEnumComponent } from './create-edit-enum/create-edit-enum.component';
 import { BrowseSuppliersComponent } from './browse-suppliers/browse-suppliers.component';
 import { CreateEditSupplierComponent } from './create-edit-supplier/create-edit-supplier.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -35,7 +36,6 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [
-    HomePageComponent,
     CreateInboundOrderComponent,
     BrowseItemsComponent,
     WarehouseBasicLayoutComponent,
@@ -43,6 +43,8 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     BrowseUsersComponent,
     CreateEditUserComponent,
     CreateEditItemComponent,
+    SettingsComponent,
+    CreateEditEnumComponent,
     BrowseSuppliersComponent,
     CreateEditSupplierComponent
   ],
